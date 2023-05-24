8f80759a1154503fc41cc7773324f685865845a8
BmihalyV <91184812+BmihalyV@users.noreply.github.com>
2023-05-24 11:23:42 +0200

    Update main.tf

--- Felhőalapú/main.tf
+++ Felhőalapú/main.tf
@@ -1,15 +1,18 @@
+module "data" {
+   source = "./modules/data"
+   file_path = module.files.example_output[0]
+   depends_on = [module.files]
+}  
+
 module "files" {
   source  = "./modules/files"
   content = "test"
- filename = "filename"
+  filename = "filename"
 }
 module "read" {
   source        = "./modules/read"
   read_variable = tostring(module.files.example_output[0])
 }
-locals {
-  answers = file("modules/write/terraform.tfvars")
-}  
  
 module "write" {
   source        = "./modules/write"
@@ -20,13 +23,11 @@ module "write" {
   answer_4 = trim(split("=", split("\n", local.answers)[3])[1], "\"\n")
   answer_5 = trim(split("=", split("\n", local.answers)[4])[1], "\"\n")
 }
-  
-module "data" {
-   source = "./modules/data"
-   file_path = module.files.example_output[0]
-   depends_on = [module.files]
+     
+locals {
+  answers = file("modules/write/terraform.tfvars")
 }  
-
+     
 output "file_id" {
    value = module.data.file_id
  }
